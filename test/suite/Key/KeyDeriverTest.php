37beba21ead87168f4af1e354de5ed482effd1c8
Erin Millard <ezzatron@gmail.com>
2014-04-15 13:08:47 +1000

    Multilingual key derivation test vectors. Closes #19.

--- test/suite/Key/KeyDeriverTest.php
+++ test/suite/Key/KeyDeriverTest.php
@@ -49,14 +49,15 @@ class KeyDeriverTest extends PHPUnit_Framework_TestCase
     {
         $this->salt = '1234567890123456789012345678901234567890123456789012345678901234';
 
-        //                           password  iterations salt         encryptionSecret                               authenticationSecret
+        //                           password                       iterations salt         encryptionSecret                               authenticationSecret
         return array(
-            'Test vector 1' => array('',       1000,      $this->salt, '2k1fkksUHSjVMxOMNkPBihtocgu1ziAI4CVRFfC7ClM', 'lNXoGLA83xvvlAUuHCQEw9OcsUloYygz2Oq4PFRMUh4'),
-            'Test vector 2' => array('foo',    1000,      $this->salt, '9eWWednk0FFnvE_NXA0uElPqBvSRDxTNNfKjj8j-w74', 'H8-n0cCupLeoCYckdGFWlwWc8GAl_XvBokZMgWbhB1U'),
-            'Test vector 3' => array('foobar', 1000,      $this->salt, 'gvP8UROn7oLyZpbguWlDryCE82uANmVHdp4cV1ZKNik', 'shiABRhWtR0nKk6uO_efWMf6yk7iZ8OnD9PjIdYJxVQ'),
-            'Test vector 4' => array('foobar', 10000,     $this->salt, 'ZYRW2br9KSzOY4KKpoEGHMXzT4PYa_CP5qPdqSkZKXI', 'Bq2Yqmr9iwi89x-DV5MUIMUmvEAXgYNhuLR0dt10jv0'),
-            'Test vector 5' => array('foobar', 100000,    $this->salt, 'Zbz3tZJjWJDGwMmer1aY1TNBW3uscUCziUpIpAF9sXw', 'pS5s8iWZBHwzf_hIIm4SMsR9dTHo2yfl2WHpa1Fp6wc'),
-            'Test vector 6' => array('foobar', 1,         $this->salt, 'nrmJyhdG9gAbFrTidwKwg5xeKBFF11wkMkJVbVsWG6A', 'cclAcqBRCzX8VMT-DkiNzHiH4emz6GT_iVVpIB84ccw'),
+            'Test vector 1' => array('',                            1000,      $this->salt, '2k1fkksUHSjVMxOMNkPBihtocgu1ziAI4CVRFfC7ClM', 'lNXoGLA83xvvlAUuHCQEw9OcsUloYygz2Oq4PFRMUh4'),
+            'Test vector 2' => array('foo',                         1000,      $this->salt, '9eWWednk0FFnvE_NXA0uElPqBvSRDxTNNfKjj8j-w74', 'H8-n0cCupLeoCYckdGFWlwWc8GAl_XvBokZMgWbhB1U'),
+            'Test vector 3' => array('foobar',                      1000,      $this->salt, 'gvP8UROn7oLyZpbguWlDryCE82uANmVHdp4cV1ZKNik', 'shiABRhWtR0nKk6uO_efWMf6yk7iZ8OnD9PjIdYJxVQ'),
+            'Test vector 4' => array('foobar',                      10000,     $this->salt, 'ZYRW2br9KSzOY4KKpoEGHMXzT4PYa_CP5qPdqSkZKXI', 'Bq2Yqmr9iwi89x-DV5MUIMUmvEAXgYNhuLR0dt10jv0'),
+            'Test vector 5' => array('foobar',                      100000,    $this->salt, 'Zbz3tZJjWJDGwMmer1aY1TNBW3uscUCziUpIpAF9sXw', 'pS5s8iWZBHwzf_hIIm4SMsR9dTHo2yfl2WHpa1Fp6wc'),
+            'Test vector 6' => array('foobar',                      1,         $this->salt, 'nrmJyhdG9gAbFrTidwKwg5xeKBFF11wkMkJVbVsWG6A', 'cclAcqBRCzX8VMT-DkiNzHiH4emz6GT_iVVpIB84ccw'),
+            'Test vector 7' => array("f\xC3\xB6\xC3\xB6b\xC3\xA4r", 1000,      $this->salt, 'kJcrKAvpBNxM5N3uIrBXjwznaAWAWkaqyhd_btIaC1Q', 'QKLqZ8Rsrm-WOWxRQwRQ2bSmKkeN00IF_C8MFSYp0Qs'),
         );
     }
 
